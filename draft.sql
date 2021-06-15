5e3ae2d1214d27efd10e205bf7de118c20456f0d
Hughzcc <hughzhao960@gmail.com>
2021-06-15 11:38:40 +0000

    ASd

--- draft.sql
+++ draft.sql
@@ -1,10 +1,10 @@
-SELECT first_name,last_name,rental_date
+SELECT first_name,last_name,rental_date,rental_id
 from rental
 INNER JOIN customer
 ON rental.customer_id = customer.customer_id
 where return_date is NULL
-ORDER BY rental_date DESC
-LIMIT 1,1;
+ORDER BY rental_date
+LIMIT 0,1;
 
 select *from rental
 where return_date is NULL
--- query1-3.sql
+++ query1-3.sql
@@ -1,7 +1,7 @@
-SELECT first_name,last_name,rental_date
+SELECT first_name,last_name,rental_date,rental_id
 from rental
 INNER JOIN customer
 ON rental.customer_id = customer.customer_id
 where return_date is NULL
-ORDER BY rental_date DESC
-LIMIT 1,1;
+ORDER BY rental_date
+LIMIT 0,1;
