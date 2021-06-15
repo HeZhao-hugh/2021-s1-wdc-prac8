640a1d21709635fa858cffc8504792baf0e78c9a
Hughzcc <hughzhao960@gmail.com>
2021-06-15 09:08:18 +0000

    AS

--- draft.sql
+++ draft.sql
@@ -1,1 +1,6 @@
- SELECT Count(film_id) from film;
+SELECT first_name,last_name,rental_date
+from rental
+INNER JOIN customer
+ON rental.customer_id = customer.customer_id
+ORDER BY rental_date
+LIMIT 1,1;
